8bf6b66e749ae4ac17d81e5f510dde876d5a2431
Tom Griffin <t.a.griffin@gmail.com>
2020-09-18 11:07:16 +1200

    Add dblclick event listener to trigger function called hide

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ function bindEventListeners (dots) {
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
     dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
