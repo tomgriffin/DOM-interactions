302f8b8b585d0f81528564d4ec33933dcffd9a09
Tom Griffin <t.a.griffin@gmail.com>
2020-09-18 10:58:38 +1200

    Add makeBlue function

--- game.js
+++ game.js
@@ -22,6 +22,10 @@ function makeGreen (evt) {
 }
 
 // CREATE FUNCTION makeBlue HERE
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 
 // CREATE FUNCTION hide HERE
 
